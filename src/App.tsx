5ff7138f97e0f8a7366d1bdb960ce3f6dbac9f01
miquelnez <miquelnez@gmail.com>
2022-10-13 10:13:29 +0200

    layout: color switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,26 @@
 import * as React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { Box, Text, Link, VStack, Code, Grid } from '@chakra-ui/react';
 import { Logo } from './Logo';
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box fontSize="xl" textAlign="center">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            fontSize="2xl"
-            href="https://chakra-ui.com"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
+  <Box fontSize="xl" textAlign="center">
+    <Grid minH="100vh" p={3}>
+      <VStack spacing={8}>
+        <Logo h="40vmin" pointerEvents="none" />
+        <Text>
+          Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+        </Text>
+        <Link
+          color="teal.500"
+          fontSize="2xl"
+          href="https://chakra-ui.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Learn Chakra
+        </Link>
+      </VStack>
+    </Grid>
+  </Box>
+  // </ChakraProvider>
 );
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
-import { ColorModeScript } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript, theme } from '@chakra-ui/react';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
+import Layout from './components/layout/layout';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 
@@ -12,7 +14,14 @@ const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
     <ColorModeScript />
-    <App />
+    <ChakraProvider theme={theme}>
+      {/* <App /> */}
+      <BrowserRouter>
+        <Layout>
+          <App />
+        </Layout>
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
