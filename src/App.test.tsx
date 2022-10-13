5566cea9742f2ef1e9331d03b7359298aac7df07
miquelnez <miquelnez@gmail.com>
2022-10-13 10:55:44 +0200

    api: app test recovered

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import { render } from './test-utils';
 import { App } from './App';
+import { MemoryRouter } from 'react-router-dom';
 
-test.skip('renders learn react link', () => {
-  render(<App />);
+test('renders learn react link', () => {
+  render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>
+  );
   const linkElement = screen.getByText(/learn chakra/i);
   expect(linkElement).toBeInTheDocument();
 });
